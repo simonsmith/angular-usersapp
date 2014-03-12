ad5a77904f7380f9cb8748483b6ffc50e046e442
Simon Smith <simon@interlopers.net>
2014-03-12 01:12:26 +0000

    Add controller tests

---
+++ test/unit/users/controllers/UsersListController.spec.js
@@ -0,0 +1,19 @@
+describe('UsersListController', function() {
+  beforeEach(module('app.users'));
+
+  var UsersListController, scope;
+  var UserServiceSpy = jasmine.createSpyObj('UserService', ['query']);
+
+  beforeEach(inject(function($controller, $rootScope) {
+    scope = $rootScope.$new();
+
+    UsersListController = $controller('UsersListController', {
+      $scope: scope,
+      UserService: UserServiceSpy
+    });
+  }));
+
+  it('should call the UserService query method on init', function() {
+    expect(UserServiceSpy.query).toHaveBeenCalled();
+  });
+});
