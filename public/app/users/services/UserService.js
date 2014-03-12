b59b70efa12bc577e32fa627699ea088adcdeb02
Simon Smith <simon@interlopers.net>
2014-03-12 00:00:35 +0000

    Formatting

--- public/app/users/services/UserService.js
+++ public/app/users/services/UserService.js
@@ -1,6 +1,6 @@
 angular.module('app.users')
-    .factory('UserService', function($resource) {
-        return $resource('/users/:id', {}, {
-            update: { method: 'PUT' }
-        });
+  .factory('UserService', function($resource) {
+    return $resource('/users/:id', {}, {
+      update: { method: 'PUT' }
     });
+  });
