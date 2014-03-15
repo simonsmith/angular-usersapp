b48664b6031da7fff2ba115770710713a8693e3d
Simon Smith <simon@interlopers.net>
2014-03-15 09:35:49 +0000

    Add UsersDeleteController spec

---
+++ test/unit/users/controllers/UsersDeleteController.spec.js
@@ -0,0 +1,47 @@
+describe('UsersDeleteController', function() {
+  beforeEach(module('app.users'));
+
+  var UsersDeleteController,
+    $controller,
+    $httpBackend,
+    $scope,
+    UserService,
+    controllerConfig;
+
+  beforeEach(inject(function(_$controller_, $rootScope, _$httpBackend_, _UserService_) {
+    $scope = $rootScope.$new();
+    $httpBackend = _$httpBackend_;
+    $controller = _$controller_;
+    UserService = _UserService_;
+
+    controllerConfig = {
+      $scope: $scope,
+      user: { _id: 12345 },
+      UserService: UserService
+    };
+  }));
+
+  describe('Deleting a single user', function() {
+    beforeEach(function() {
+      spyOn(UserService, 'delete').and.callThrough();
+      $scope.$close = jasmine.createSpy('$close');
+
+      $httpBackend.expectDELETE('/users/12345').respond(200);
+      UsersDeleteController = $controller('UsersDeleteController', controllerConfig);
+    });
+
+    it('should delete the user', function() {
+      $scope.deleteUser();
+      $httpBackend.flush();
+
+      expect(UserService.delete.calls.mostRecent().args[0]).toEqual({ id: 12345 });
+    });
+
+    it('should call $scope.$close() after delete', function() {
+      $scope.deleteUser();
+      $httpBackend.flush();
+
+      expect($scope.$close).toHaveBeenCalled();
+    });
+  });
+});
