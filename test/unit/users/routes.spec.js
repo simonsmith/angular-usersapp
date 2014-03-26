75951b7c404ec972623091fa384390dd56c7785f
Simon Smith <simon@interlopers.net>
2014-03-26 16:37:07 +0000

    Simplify route tests

--- test/unit/users/routes.spec.js
+++ test/unit/users/routes.spec.js
@@ -1,55 +1,33 @@
 describe('User routes', function() {
   beforeEach(module('app.users'));
 
-  var $location,
-      $route,
-      $rootScope,
-      $httpBackend;
+  var $route;
 
-  beforeEach(inject(function(_$location_, _$route_, _$rootScope_, _$httpBackend_) {
-    $location = _$location_;
+  beforeEach(inject(function(_$route_) {
     $route = _$route_;
-    $rootScope = _$rootScope_;
-    $httpBackend = _$httpBackend_;
   }));
 
-  it('should load /users', function() {
-    $httpBackend.whenGET('/public/app/users/views/user-list.html').respond(200);
-    $location.path('/users');
-    $rootScope.$digest();
-
-    expect($route.current.controller).toBe('UsersListController');
+  it('should configure /users', function() {
+    expect($route.routes['/users'].templateUrl).toBe('/public/app/users/views/user-list.html');
+    expect($route.routes['/users'].controller).toBe('UsersListController');
   });
 
-  it('should load /users/new', function() {
-    $httpBackend.whenGET('/public/app/users/views/user-edit.html').respond(200);
-    $location.path('/users/new');
-    $rootScope.$digest();
-
-    expect($route.current.controller).toBe('UsersNewController');
+  it('should configure /users/new', function() {
+    expect($route.routes['/users/new'].templateUrl).toBe('/public/app/users/views/user-edit.html');
+    expect($route.routes['/users/new'].controller).toBe('UsersNewController');
   });
 
-  it('should load /users/edit/:id', function() {
-    $httpBackend.whenGET('/public/app/users/views/user-edit.html').respond(200);
-    $location.path('/users/edit/12345');
-    $rootScope.$digest();
-
-    expect($route.current.controller).toBe('UsersEditController');
+  it('should configure /users/edit/:userId', function() {
+    expect($route.routes['/users/edit/:userId'].templateUrl).toBe('/public/app/users/views/user-edit.html');
+    expect($route.routes['/users/edit/:userId'].controller).toBe('UsersEditController');
   });
 
-  it('should load /users/:id', function() {
-    $httpBackend.whenGET('/public/app/users/views/user-view.html').respond(200);
-    $location.path('/users/12345');
-    $rootScope.$digest();
-
-    expect($route.current.controller).toBe('UsersViewController');
+  it('should configure /users/:userId', function() {
+    expect($route.routes['/users/:userId'].templateUrl).toBe('/public/app/users/views/user-view.html');
+    expect($route.routes['/users/:userId'].controller).toBe('UsersViewController');
   });
 
   it('should redirect to the /users when no route is found', function() {
-    $httpBackend.whenGET('/public/app/users/views/user-list.html').respond(200);
-    $location.path('/some/wrong/route');
-    $rootScope.$digest();
-
-    expect($route.current.controller).toBe('UsersListController');
+    expect($route.routes['null'].redirectTo).toBe('/users');
   });
 });
